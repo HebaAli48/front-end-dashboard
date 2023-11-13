afe03279d3455b90c9c26222143a24a49f8b66a2
HebaAli48 <131808003+HebaAli48@users.noreply.github.com>
2023-11-13 11:09:28 +0200

    added profile tab in header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   const headerData = [
     { title: "Home", to: "/" },
     { title: "About", to: "/About" },
-    { title: "Support", to: "/Support" },
+    { title: "profile", to: "/user" },
   ];
   return (
     <nav
@@ -56,60 +56,6 @@ const Header = () => {
                 </NavLink>
               </li>
             ))}
-
-            {/* {isAuthenticated && !isAdmin && (
-                <li className="pt-2 sm:pt-0">
-                  <Link
-                    to="/wishlist"
-                    className="hover:border-b-2 border-line-primary py-1 transition"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill={`${
-                        wishlist.length > 0 ? "var(--btn-primary)" : "none"
-                      }`}
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke={`${
-                        wishlist.length > 0 ? "var(--btn-primary)" : "white"
-                      }`}
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                      />
-                    </svg>
-                  </Link>
-                </li>
-              )}
-              {!isAdmin && (
-                <li className="  pt-2 sm:pt-0 relative">
-                  <Link to="/cart">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      // fill={cartNumber > 0 ? "var(--btn-primary)" : "none"}
-                      fill={cartNumber > 0 ? "none" : "none"}
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                      />
-                    </svg>
-                  </Link>
-                  {cartNumber > 0 && (
-                    <span className="bg-btn-primary text-white text-xs font-medium mr-2 w-4 h-4 rounded-full dark:bg-btn-primary dark:text-btn-primary absolute  top-[-2px] left-[15px] sm:top-[-11px] text-center">
-                      {cartNumber}
-                    </span>
-                  )}
-                </li>
-              )} */}
           </ul>
         </div>
         <div className="flex gap-2 sm:gap-5 items-center justify-end sm:mr-5 ml-auto">
